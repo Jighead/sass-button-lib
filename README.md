204169ffd481ab2e8da7261fbb10e7bf5d775fb7
Gerd <gerdsuhr@gmail.com>
2018-06-04 15:43:06 -0400

    First commit.  Straight to master.

--- README.md
+++ README.md
@@ -1,26 +1,7 @@
-# Gulp scaffold with ES6, Browserify and Babel as well as jQuery, Sass baked in.
-This is a gulp work-flow / scaffold that I often use that lets you to build ES6 projects using ES6 Export/Import modules.
-It also has examples of how you can create moduler render components and inject them into the dom much like you would in React or Vue. 
+# Sass Button Theme library.
 
-Uses the following gulp packages
-
-    * gulp-print - prints the gulp tasks if you want to see them working
-    * del - allows us to delete files/folders
-    * gulp-run-sequence - allows us to run gulp tasks in specific sequence
-    * gul-browserify - creates wrappper around our JS so we can us modules and the browser understands it.
-    * gulp-babelify - transpiles our JS to ES6 (with Browserify we need babelify not babel, See .babelrc file)
-    * gup-vinyl-source-stream - we need viny-source-stream to be able to pipe within the browserify task
-    * gulp-util' - logging tool for debuggin you gulp tasks
-    * gulp-sass' - transpile scss code to css
-    * gulp-autoprefixer autoprefix our css for across browsers
-    * gulp-concat - concatenate files
-    * gulp-clean-css - clean and minify css
-    * gulp-sourcemaps - create css sourcemaps
-    * gulp-uglify - minify javascript
-    * gulp-streamify - we need streamify to be able to pipe uglify in browserify task
-    * gulp-order - allows us to specifify the order to concatenate our css
-    * browser-sync - dev server (hot reloading with watch)
-
+SASS is documented! Enjoy
+ 
 ## usage
 
 * clone repo or download the zip file
@@ -37,4 +18,4 @@ gulp dev     -- To stop server, press CTRL+C
 gulp     -- build for production (no server)
 ```
 
-## Write some kick-ass code! 
+## Use it in your next SASS project!
--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,7 @@ var gulp = require('gulp'),
 		runSq = require('run-sequence'), 
 		// creates wrappper around our JS so we can us modules and the browser understands it.
 		browserify = require("browserify"),
-		// babelify transpiles our JS to ES6 (with Browserify we need babelify not babel, See .babelrc file)
-		babelify = require("babelify"),
-		// we need viny-source-stream to be able to pipe within the browserify task
+	// we need viny-source-stream to be able to pipe within the browserify task
 		source = require('vinyl-source-stream'),
 		// gulp-util is a logging tool for debuggin you gulp tasks
 		gutil = require('gulp-util'),
@@ -42,18 +40,7 @@ gulp.task('cleanOut', () => {
 	return del([  'dist/**/*',  ]) 
 });
 
-// Browserify / Babelify - transpiles our code to ES6 and places it in a wrapper 
-// We are also minifying
-gulp.task('transpile', function() {
-	return browserify({ debug: true })
-		.transform(babelify)
-		.require("./src/app.js", { entry: true })
-		.bundle()
-		.on('error',gutil.log) // log errors to console
-		.pipe(source('bundle.js')) // save it as bundle.js 
-		.pipe(streamify(uglify())) // minifiy it
-    .pipe(gulp.dest('./dist')); // save it in this folder
-});
+
 
 // Convert sass into css
 gulp.task('sass', () => {
@@ -66,21 +53,6 @@ gulp.task('sass', () => {
 		.pipe(gulp.dest('dist/css')) // save css here
 });
 
-// concatenate and minify all our 3rd party vendor css into 1 file.
-gulp.task('vendor-css', () => {
-	return gulp.src('./src/css/*.css')
-	//.pipe(print())
-					.pipe(autoprefixer()) // prefix the css
-					.pipe(order([
-						"css/boostrap3.js",
-						"gulp css/animate.css",
-						//"./css/**/font-awesome.css"
-					]))
-					.pipe(concat('vendor.css')) // concatenate into 1 file called vendor.css
-					.pipe(cleanCss()) // minify the css
-					.pipe(gulp.dest('./dist/css')) // save it here
-})
-
 // copy all html files to dist folder
 gulp.task('copy-html', () => {
 	return gulp.src('./src/**/*.html') // glob the folder structure
@@ -88,32 +60,24 @@ gulp.task('copy-html', () => {
 	.pipe(gulp.dest('dist')) 
 });
 
-// copy all image files to dist/images folder
-gulp.task('copy-images', () => {
-	return gulp.src('./src/images/**/*.*') // glob the folder structure
-	//.pipe(print())
-	.pipe(gulp.dest('./dist/images')) 
-})
 
 // launching browser-sync server with watch tasks
 gulp.task('server', function() {
 	browserSync.init({
 			server: "./dist"
 	});
-	gulp.watch('src/css/*.css', ['vendor-css']).on('change', browserSync.reload );
+
 	gulp.watch("src/scss/*.scss", ['sass']).on('change', browserSync.reload );
-	gulp.watch("./src/**/*.js", ['transpile']).on('change', browserSync.reload);
-	gulp.watch("src/images/**/*.*", ['copy-images']).on('change', browserSync.reload);
 	gulp.watch("src/**/*.html", ['copy-html'] ).on('change', browserSync.reload);
 });
 
 // run these tasks in sequence using run-sequence and launce the dev server
 gulp.task('dev', function() {
-	runSq('cleanOut', ['copy-html', 'copy-images', 'vendor-css'], ['sass', 'transpile','server']);
+	runSq('cleanOut', ['copy-html'], ['sass', 'server']);
 });
 
 // Use for Production build.
 gulp.task('default', function() {
-	runSq('cleanOut', ['copy-html', 'copy-images', 'vendor-css'], ['sass', 'transpile']);
+	runSq('cleanOut', ['copy-html'], ['sass']);
 });
 
